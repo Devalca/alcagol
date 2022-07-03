6957d5d9252fc45222fa81b6a3c6e3254a7bbc03
Amatoxin404 <saepulrahman404@gmail.com>
2022-07-03 20:48:11 +0700

    news

--- app/Http/Controllers/SalaryGroupController.php
+++ app/Http/Controllers/SalaryGroupController.php
@@ -86,33 +86,6 @@ class SalaryGroupController extends Controller
                 $salaryGroup->save();
                 return redirect('/salary_groups')->with('success', 'Data Berhasil Ditambahkan');
             }
-            
-
-            // $gaji_pokok_min = SalaryGroup::latest()->pluck('gaji_pokok')->first();
-            // $gaji_pokok_const = SalaryGroup::oldest()->pluck('gaji_pokok')->first();
-            // $persentase_kenaikan = $salaryGroup->persentase_kenaikan;
-            // $id_golongan = $salaryGroup->id_golongan;
-            // $gaji_pokok = 
-            // ($gaji_pokok_min + ($gaji_pokok_const * (($persentase_kenaikan / 6) * ($id_golongan + 1)) / 100 ));
-            // $salaryGroup->gaji_pokok = $gaji_pokok;
-            
-    
-        // $basic_salary = ($id_salarygroup="1A1"->$basic_salary);
-        // $percentage = $id_seconddigit->percentage;
-        // $basic_salary_a = $basic_salary + ( $basic_salary * ( ( $percentage / 6) * 1) % );
-        // $salaryGroup->basic_salary = $basic_salary_a;
-        
-        
-        // //	Start Third Digit Case
-        // $salaryGroupFirst = SalaryGroup::where('id_thirddigit' == 1)->first();
-    
-        // if ($salaryGroupFirst->exists()) {
-        //     $salaryGroup->basic_salary = ($request->input('basic_salary') * 
-        //     ($salaryGroupFirst->percentage * $salaryGroupFirst->id - 1)) + $request->input('basic_salary');
-        // } else {
-        //     $salaryGroup->basic_salary = $request->input('basic_salary');
-        // }
-        // //	End Third Digit Case
     
     }
 
@@ -155,19 +128,8 @@ class SalaryGroupController extends Controller
             'gaji_pokok' => 'required',
         ]);
 
-        // Create Data
         $salaryGroup = SalaryGroup::find($id_golongan);
-        // $salaryGroup->nama_golongan = $request->input('nama_golongan');
-        // $salaryGroup->persentase_kenaikan = $request->input('persentase_kenaikan');
         $salaryGroup->gaji_pokok = $request->input('gaji_pokok');
-
-        // $gaji_pokok_min = SalaryGroup::latest()->pluck('gaji_pokok')->first();
-        //     $gaji_pokok_const = SalaryGroup::oldest()->pluck('gaji_pokok')->first();
-        //     $persentase_kenaikan = $salaryGroup->persentase_kenaikan;
-        //     $id_golongan = $salaryGroup->id_golongan;
-        //     $gaji_pokok = 
-        //     ($gaji_pokok_min + ($gaji_pokok_const * (($persentase_kenaikan / 6) * ($id_golongan + 1)) / 100 ));
-        //     $salaryGroup->gaji_pokok = $gaji_pokok;
 
         $salaryGroup->save();
         return redirect('/salary_groups')->with('success', 'Data Berhasil Diperbaharui');
@@ -185,39 +147,6 @@ class SalaryGroupController extends Controller
         $salaryGroup->delete();
         return redirect('/salary_groups')->with('success', 'Data Berhasil Dihapus');
     }
-
-    // public function destroy_all()
-    // {
-    //     DELETE * FROM 'salary_groups';
-    //         WHERE condition;
-          
-        
-
-    //     return redirect('/salary_groups')->with('success', 'Data Berhasil Dihapus');
-    // }
-
-    // public function edit_presentase($id_golongan)
-    // {
-    //     $salary_group = DB::table('salary_groups')->where($id_golongan)->'1';
-    //     // $salary_group = DB::table('salary_groups')->where('nama_golongan', $nama_golongan);
-    //     return view('salary_groups.edit_presentase')->with('salary_group', $salary_group);
-    // }
-
-    // public function update_presentase(Request $request)
-    // {
-    //     $salaryGroup = new SalaryGroup();
-        
-    //         $this->validate($request, [
-    //             'persentase_kenaikan' => 'required',
-    //         ]);
-        
-    //         $salaryGroup->persentase_kenaikan = $request->input('persentase_kenaikan');
-    
-    //     $salaryGroup->save();
-    //     return redirect('/salary_groups')->with('success', 'Data Berhasil Ditambahkan');
-    
-    // }
-
 
 }
 
